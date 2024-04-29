078312e9d29693327ae603eb99b366978f6bcc0e
shunotsuda <shun1020ymsn@gmail.com>
2024-04-29 16:12:06 +0900

    layout調整

--- src/app/(main)/message/page.tsx
+++ src/app/(main)/message/page.tsx
@@ -8,19 +8,22 @@ const dancingScript = Dancing_Script({ subsets: ["latin"] });
 const Message = () => {
     return (
         <div className="m-4 md:p-10 text-gray-800 h-full overflow-y-auto overflow-x-hidden">
-            <header className=" w-full flex flex-col md:flex-row justify-between items-center bg-white pb-4 max-w-[980px] m-auto">
+           <div className="flex justify-end mb-4">
+                <Link
+                        href="/message/new"
+                        className=" flex items-center gap-1 text-[1rem] md:text-sm md:font-semibold border px-2 md:px-4 py-2 md:py-3 mr-4 rounded-full shadow-sm text-white hover:text-opacity-75 bg-gray-800 hover:bg-gray-700"
+                    >
+                        <IoIosCreate className="md:size-5" />
+                        <div className="border-b border-green-500">New Message</div>
+                    </Link>
+           </div>
+            <header className=" w-full  bg-white pb-4 max-w-[980px] m-auto">
                 <h1
-                    className={`${dancingScript.className} text-[3rem] lg:text-[4rem] font-bold ml-4 md:ml-4`}
+                    className={`${dancingScript.className} text-center text-[3rem] lg:text-[4rem] font-bold`}
                 >
                     ~ Message ~
                 </h1>
-                <Link
-                    href="/message/new"
-                    className=" flex items-center gap-1 text-[1rem] md:text-sm md:font-semibold border px-2 md:px-4 py-2 md:py-3 mr-4 rounded-full shadow-sm text-white hover:text-opacity-75 bg-gray-800 hover:bg-gray-700"
-                >
-                    <IoIosCreate className="md:size-5" />
-                    <div className="border-b border-green-500">New Message</div>
-                </Link>
+                
             </header>
             <div className="bg-white px-8 py-4 max-w-[980px] m-auto">
                 <div className="border-b  border-green-500">
