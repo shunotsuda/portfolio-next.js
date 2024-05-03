60a077f3c8e8ecf5b7a6f73fe2a04bf81026c578
shunotsuda <shun1020ymsn@gmail.com>
2024-05-03 18:08:39 +0900

    sinisou

--- src/app/(main)/message/page.tsx
+++ src/app/(main)/message/page.tsx
@@ -6,20 +6,21 @@ import { Dancing_Script } from "next/font/google";
 
 const dancingScript = Dancing_Script({ subsets: ["latin"] });
 
-const getAllMessages = async (): Promise<MessageDocument[]> => {
-    const response = await fetch(`https://portfolio-next-js-beta-eight.vercel.app/api/messages`, {
-        cache:"no-store",
-    });
 
-    if (response.status !== 200) {
-        throw new Error();
-    }
-
-    const data = await response.json();
-    return data.messages as MessageDocument[];
-};
 
 export default async function Message() {
+    const getAllMessages = async (): Promise<MessageDocument[]> => {
+        const response = await fetch(`${process.env.API_URL}/messages`, {
+            cache:"no-store",
+        });
+    
+        if (response.status !== 200) {
+            throw new Error();
+        }
+    
+        const data = await response.json();
+        return data.messages as MessageDocument[];
+    };
     const allMessages = await getAllMessages();
     return (
         <div className="m-4 md:p-10 text-gray-800 h-full overflow-y-auto overflow-x-hidden">
@@ -32,9 +33,7 @@ export default async function Message() {
                 >
                     ~ Message ~
                 </h1>
-                {allMessages.map((message) => (
-                    <MessageCard key={message._id} message={message} />
-                ))}
+                {allMessages.map((message) => <MessageCard key={message._id} message={message} />)}
             </div>
         </div>
     );
